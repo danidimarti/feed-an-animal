1268d5eabfa139e43bc8f6d103f96bdd299afad9
Daniela Demarchi <daniela.demarchi@four-paws.org>
2019-04-18 12:22:13 +0200

    no developments

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,3 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 }, false);
 
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
